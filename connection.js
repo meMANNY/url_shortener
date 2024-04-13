803af3c8b93cfaacfa70367215f5b7bef3fd8be9
Aman Tripathi <amanttripathi02@gmail.com>
2024-04-13 16:05:27 +0530

    "created the logic to redirect to original url"

--- connection.js
+++ connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+mongoose.set("strictQuery", true);
 async function connectdb(url){
     return mongoose.connect(url);
 }
--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const urlRoute = require("./routes/url");
 const app = express();
-
+const Url = require('./models/url');
 const {connectdb} = require('./connection');
 
 const PORT = 8001;
 
 connectdb('mongodb://localhost:27017/url-shortener');
 
+app.use(express.json());
 app.use("/url", urlRoute);
+
+app.get("/:shortId", async(req,res)=>{
+    const shortId = req.params.shortId;
+    const url = await Url.findOneAndUpdate({
+        shortId: shortId
+    },{
+        $push: {visitHistory: {timestamp:Date.now()}}
+    })
+
+    res.redirect(url.redirectUrl);
+})
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
